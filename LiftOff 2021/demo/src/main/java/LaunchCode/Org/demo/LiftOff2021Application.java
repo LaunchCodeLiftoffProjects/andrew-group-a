100cd018a1284ad60a0f90e332de7c1fb669ff01
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-08-16 19:32:31 -0500

    Added Spring Project

---
+++ LiftOff 2021/demo/src/main/java/LaunchCode/Org/demo/LiftOff2021Application.java
@@ -0,0 +1,13 @@
+package LaunchCode.Org.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftOff2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftOff2021Application.class, args);
+	}
+
+}
---
+++ LiftOff 2021/demo/src/test/java/LaunchCode/Org/demo/LiftOff2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package LaunchCode.Org.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftOff2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
