b09633111dc1337b17a079ef9dd4172047227d03
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-08-16 19:47:11 -0500

    Added Spring Project

---
+++ LiftOff2021/LiftOff2021/src/main/java/Org/LaunchCode/LiftOff2021/LiftOff2021Application.java
@@ -0,0 +1,13 @@
+package Org.LaunchCode.LiftOff2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftOff2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftOff2021Application.class, args);
+	}
+
+}
