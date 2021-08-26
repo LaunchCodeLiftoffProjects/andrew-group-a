7d9abb3158af21c221dea34a89c84859e7e76e41
nsteil <78703777+nsteil@users.noreply.github.com>
2021-08-26 18:39:14 -0500

    Disabled csrf in the HTTPSecurity configure

--- src/main/java/org/launchcode/andrewgroupa/security/WebSecurity.java
+++ src/main/java/org/launchcode/andrewgroupa/security/WebSecurity.java
@@ -33,7 +33,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
 
-                    .antMatchers("/", "/home", "/**.css","/list").permitAll()
+                    .antMatchers("/", "/home", "/**.css").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
