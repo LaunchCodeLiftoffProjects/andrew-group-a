67b8282d474fcecd545f7ebe7f7afb437f96c9a8
Divyapancholi831 <divyapancholi1995@gmail.com>
2021-08-26 01:13:56 -0500

    add model , controller and repository for tag..able to add tags but relationship with list is still left

--- src/main/java/org/launchcode/andrewgroupa/controllers/TagController.java
+++ src/main/java/org/launchcode/andrewgroupa/controllers/TagController.java
@@ -1,10 +1,17 @@
 package org.launchcode.andrewgroupa.controllers;
 
 import org.launchcode.andrewgroupa.data.TagRepository;
+import org.launchcode.andrewgroupa.models.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("tags")
@@ -13,9 +20,25 @@ public class TagController {
     @Autowired
     TagRepository tagRepository;
 
-    public String displayTag(Model model){
+    @GetMapping
+    public String displayTagAndAddTagForm(Model model){
         model.addAttribute("title","All Tags");
+        model.addAttribute(new Tag());
         model.addAttribute("tags",tagRepository.findAll());
         return "tag/index";
     }
+
+    @PostMapping
+    public String processAddTagForm(@ModelAttribute @Valid Tag newTag,Errors errors,Model model)
+    {
+        if (errors.hasErrors())
+        {
+            model.addAttribute("title", "All Tags");
+            return "tag/index";
+        }
+
+        tagRepository.save(newTag);
+        return "redirect:/tags";
+    }
+
 }
