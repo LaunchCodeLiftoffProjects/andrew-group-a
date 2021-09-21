5192f3cafd9c626de72d1f35072ea7b86ab11645
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-09-20 19:49:48 -0500

    Try and catch ... somewhat working... need to do something with error

--- src/main/java/org/launchcode/andrewgroupa/models/User.java
+++ src/main/java/org/launchcode/andrewgroupa/models/User.java
@@ -1,62 +1,72 @@
 package org.launchcode.andrewgroupa.models;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import com.sun.istack.NotNull;
+import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User extends AbstractEntity {
 
-  @OneToMany(mappedBy = "listOwner")
-  private final List<ShoppingList> shoppingLists = new ArrayList<>();
-  @NotNull
-  @NotBlank
-  @Column(unique = true)
-  private String username;
-  @NotBlank
-  @NotNull
-  private String password;
-  private boolean active;
-  private String roles;
+    @NotNull
+    @NotBlank
+    @Column(unique = true)
+    private String username;
 
-  public String getUsername() {
-    return username;
-  }
+    @NotBlank
+    @NotNull
+    private String password;
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    private boolean active;
 
-  public String getPassword() {
-    return password;
-  }
+    private String roles;
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    @OneToMany(mappedBy = "listOwner")
+    private final List<ShoppingList> shoppingLists = new ArrayList<>();
 
-  public boolean isActive() {
-    return active;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public void setActive(boolean active) {
-    this.active = active;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-  public String getRoles() {
-    return roles;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setRoles(String roles) {
-    this.roles = roles;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public List<ShoppingList> getShoppingLists() {
-    return shoppingLists;
-  }
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public List<ShoppingList> getShoppingLists() {
+        return shoppingLists;
+    }
 }
