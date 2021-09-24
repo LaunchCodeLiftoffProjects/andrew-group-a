a4c6e8e6a46028e035668013baf4fab86764238f
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-09-23 19:50:41 -0500

    Haven't quite figured out the try and catch problem.

--- src/main/java/org/launchcode/andrewgroupa/controllers/HomeController.java
+++ src/main/java/org/launchcode/andrewgroupa/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package org.launchcode.andrewgroupa.controllers;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
