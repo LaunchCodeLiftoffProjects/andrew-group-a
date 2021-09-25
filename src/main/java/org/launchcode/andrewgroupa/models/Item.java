f87e6501dccf89266cf3950ef0c54f8b5594f731
Justin Portell <justinportell84@gmail.com>
2021-09-24 19:51:38 -0500

    Quantity.

--- src/main/java/org/launchcode/andrewgroupa/models/Item.java
+++ src/main/java/org/launchcode/andrewgroupa/models/Item.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +13,22 @@ import java.util.List;
 @Entity
 public class Item extends AbstractEntity {
 
-    @NotBlank
-    @Size(min = 3, max = 20, message = "Item name but be between 3 and 20 characters.")
-    @Column(unique = true)
-    private String name;
-
-
-    @ManyToOne
-    private ShoppingList shoppingList;
+  @ManyToMany
+  private final List<Tag> tags = new ArrayList<>();
+  @NotBlank
+  @Size(min = 3, max = 20, message = "Item name but be between 3 and 20 characters.")
+  @Column(unique = true)
+  private String name;
+  @ManyToOne
+  private ShoppingList shoppingList;
+  private int quantity;
+  private boolean complete;
 
     public Item(String name, ShoppingList shoppingList) {
         this.name = name;
         this.shoppingList = shoppingList;
+        this.quantity = 1;
+        this.complete = false;
     }
 
 
@@ -33,38 +36,42 @@ public class Item extends AbstractEntity {
     private final List<Tag> tags = new ArrayList<>();
 
 
-    public Item() {}
-
-    public Item(String name) {
-        this.name = name;
+    public Item() {
+        this.complete = false;
+        this.quantity = 1;
     }
 
+  public int getQuantity() {
+    return quantity;
+  }
 
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public ShoppingList getShoppingList() {
-        return shoppingList;
-    }
+  public ShoppingList getShoppingList() {
+    return shoppingList;
+  }
 
-    public void setShoppingList(ShoppingList shoppingList) {
-        this.shoppingList = shoppingList;
-    }
+  public void setShoppingList(ShoppingList shoppingList) {
+    this.shoppingList = shoppingList;
+  }
 
-    public List<Tag> getTags() {
-        return tags;
-    }
+  public List<Tag> getTags() {
+    return tags;
+  }
 
-    public void addTag(Tag tag){
-        this.tags.add(tag);
-    }
-
+  public void addTag(Tag tag) {
+    this.tags.add(tag);
+  }
 
 
 }
