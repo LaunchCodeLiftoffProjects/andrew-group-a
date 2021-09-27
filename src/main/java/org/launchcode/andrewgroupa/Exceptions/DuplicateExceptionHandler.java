ce1faf1ffd9223acb7184f61f431c91933aa4cbe
Justin Portell <justinportell84@gmail.com>
2021-09-27 12:10:52 -0500

    .

--- src/main/java/org/launchcode/andrewgroupa/Exceptions/DuplicateExceptionHandler.java
+++ src/main/java/org/launchcode/andrewgroupa/Exceptions/DuplicateExceptionHandler.java
@@ -1,30 +1,29 @@
 package org.launchcode.andrewgroupa.Exceptions;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
 @ControllerAdvice
 public class DuplicateExceptionHandler {
 
-    @ExceptionHandler(value = {DuplicateUser.class})
-    public ResponseEntity<Object> handleDuplicateUser(DuplicateUser e) {
-        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+  @ExceptionHandler(value = {DuplicateUser.class})
+  public ResponseEntity<Object> handleDuplicateUser(DuplicateUser e) {
+    HttpStatus badRequest = HttpStatus.BAD_REQUEST;
 
-        DuplicateException duplicateException = new DuplicateException(
-                e.getMessage(),
-                e,
-                badRequest,
-                ZonedDateTime.now(ZoneId.of("Z"))
-        );
+    DuplicateException duplicateException = new DuplicateException(
+        e.getMessage(),
+        e,
+        badRequest,
+        ZonedDateTime.now(ZoneId.of("Z"))
+    );
 
-        System.out.println("There was an error here");
+    System.out.println("There was an error here");
 
-        return new ResponseEntity<>(duplicateException, badRequest);
-    }
+    return new ResponseEntity<>(duplicateException, badRequest);
+  }
 
 }
--- src/main/java/org/launchcode/andrewgroupa/controllers/RegistrationController.java
+++ src/main/java/org/launchcode/andrewgroupa/controllers/RegistrationController.java
@@ -1,12 +1,9 @@
 package org.launchcode.andrewgroupa.controllers;
 
-import org.hibernate.NonUniqueObjectException;
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
-import org.launchcode.andrewgroupa.Exceptions.DuplicateException;
-import org.launchcode.andrewgroupa.Exceptions.DuplicateUser;
 import org.launchcode.andrewgroupa.data.UserRepository;
 import org.launchcode.andrewgroupa.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,38 +13,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.sql.SQLException;
 
 @Controller
 @RequestMapping("register")
 public class RegistrationController {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @GetMapping
-    public String displayRegistrationForm (){
-        return "registration";
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @GetMapping
+  public String displayRegistrationForm() {
+    return "registration";
+  }
+
+  @PostMapping
+  public String finishRegistration(@ModelAttribute @Valid User newUser, Errors errors,
+      Model model) {
+
+    if (errors.hasErrors()) {
+      return "/registration?error";
     }
 
-    @PostMapping
-    public String finishRegistration(@ModelAttribute @Valid User newUser, Errors errors, Model model){
-
-        if (errors.hasErrors()) {
-            return "/registration?error";
-        }
-
-
-        try {
-            newUser.setRoles("USER");
-            newUser.setActive(true);
-            userRepository.save(newUser);
-            return "redirect:/login";
-        } catch (Exception e){
-            System.out.println("Caught" + e.getMessage());
-            return "redirect:/register?error";
-        }
-
+    try {
+      newUser.setRoles("USER");
+      newUser.setActive(true);
+      newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+      userRepository.save(newUser);
+      return "redirect:/login";
+    } catch (Exception e) {
+      System.out.println("Caught" + e.getMessage());
+      return "redirect:/register?error";
     }
+
+  }
 
 }
